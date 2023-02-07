50987d2b682c2cbdf8d7dc7a6e013e56ca5e74e5
sirio95 <sirio.iasevoli@gmail.com>
2023-02-07 09:49:06 +0100

    migrate people

---
+++ database/migrations/2023_02_07_084530_create_people_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->integer('id')->primary()->unique();
+            $table->string('firstname', 32);
+            $table->string('lastname', 32);
+            $table->date('birth_date');
+            $table->integer('height')->nullable()->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('people');
+    }
+};
